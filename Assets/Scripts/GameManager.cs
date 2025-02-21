53d7c7599df7ad77df75cd0a133a6f3f3a7512f2
Janxyxy <jankroupa2@gmail.com>
2025-02-21 21:56:03 +0100

    Prefabs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   public GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
