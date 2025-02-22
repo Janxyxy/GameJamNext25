cf31e12b89f9ce8edf2337e5abb9c3816cfd68f0
Janxyxy <jankroupa2@gmail.com>
2025-02-22 23:25:27 +0100

    edhfuj

---
+++ Assets/Scripts/Room/GymRoom.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GymRoom : Room
+{
+    protected override void OnAddAntsClick()
+    {
+
+    }
+
+    protected override void OnRemoveAntsClick()
+    {
+
+    }
+}
--- Assets/Scripts/RoomData.cs
+++ Assets/Scripts/RoomData.cs
@@ -2,4 +2,19 @@
 public class RoomData
 {
     public int antsCount;
+
+    public void AddAnt(int count)
+    {
+        antsCount += count;
+    }
+
+    public bool RemoveAnt(int count)
+    {
+        if (antsCount >= count)
+        {
+            antsCount -= count;
+            return true;
+        }
+        return false;
+    }
 }
